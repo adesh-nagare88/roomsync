f3a413e08d50977db6353213e69aea81c462c22c
Adesh-nagare <anagare@algoanalytics.com>
2025-07-20 18:07:25 +0530

    Deployment Stage

--- server/server.js
+++ server/server.js
@@ -11,11 +11,10 @@ const noticeRoutes = require("./routes/noticeRoutes");
 const reminderRoutes = require("./routes/reminderRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: [process.env.FRONTEND_URL ||"http://localhost:3000"],
   credentials: true,              
 }));
 app.use(express.json());
@@ -30,10 +29,14 @@ app.use("/api/notices", noticeRoutes);
 app.use("/api/reminders", reminderRoutes);
 
 // DB + Server
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error("MongoDB connection error:", err));
+let isConnected = false;
+async function connectDB() {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGO_URI);
+  isConnected = true;
+  console.log("MongoDB connected");
+}
+connectDB();
+
+
+module.exports = app;
